349a0fd43ef6af84edd5805f07672ad84078887b
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-21 13:11:36 +0200

    Started to work on PostDetails

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core/';
+import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
+import { useParams, useHistory } from 'react-router-dom';
 
-const PostDetails = () => {
+import { getPost, getPostsBySearch } from '../../actions/posts';
+import useStyles from './styles';
+
+const Post = () => {
+  const { post, posts, isLoading } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const classes = useStyles();
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(getPost(id));
+  }, [id]);
+
+  useEffect(() => {
+    if (post) {
+      dispatch(getPostsBySearch({ search: 'none', tags: post?.tags.join(',') }));
+    }
+  }, [post]);
+
+  if (!post) return null;
+
+  const openPost = (_id) => history.push(`/posts/${_id}`);
+
+  if (isLoading) {
     return (
-        <div>
-            
-        </div>
-    )
-}
+      <Paper elevation={6} className={classes.loadingPaper}>
+        <CircularProgress size="7em" />
+      </Paper>
+    );
+  }
 
-export default PostDetails
+  const recommendedPosts = posts.filter(({ _id }) => _id !== post._id);
+
+  return (
+    <div></div>
+  );
+};
+
+export default Post;
